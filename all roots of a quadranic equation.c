dcc3ffcfefe34286572d79a58c92d23e85fbbb51
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 20:42:49 +0600

    all roots of a quadranic equation.c

--- all roots of a quadranic equation.c
+++ all roots of a quadranic equation.c
@@ -1,1 +1,44 @@
 
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+    float a, b, c, D, root1, root2, realPart, imagPart;
+
+    printf("Enter coefficients a, b and c: ");
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+
+    D = (b * b) - (4 * a * c);  
+
+    if (D > 0) {
+       
+        root1 = (-b + sqrt(D)) / (2 * a);
+        root2 = (-b - sqrt(D)) / (2 * a);
+        printf("Roots are real and distinct.\n");
+        printf("Root1 = %.2f\n", root1);
+        printf("Root2 = %.2f\n", root2);
+      
+
+    } else if (D == 0) {
+       
+        root1 = root2 = -b / (2 * a);
+        printf("Roots are real and equal.\n");
+        printf("Root1 = Root2 = %.2f\n", root1);
+      
+
+    } else {
+        
+        realPart = -b / (2 * a);
+        imagPart = sqrt(-D) / (2 * a);
+        printf("Roots are complex and imaginary.\n");
+        printf("Root1 = %.2f + %.2fi\n", realPart, imagPart);
+        printf("Root2 = %.2f - %.2fi\n", realPart, imagPart);
+      
+    }
+
+    return 0;
+}
