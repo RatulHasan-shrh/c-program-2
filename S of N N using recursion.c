9a1adc8a89d085b0d5f1bf4a0a0fb3a6851b074b
RatulHasan-shrh <ratulhasan2603@gmail.com>
2025-11-19 20:55:28 +0600

    S of N N using recursion.c

--- S of N N using recursion.c
+++ S of N N using recursion.c
@@ -1,1 +1,22 @@
 
+
+#include <stdio.h>
+
+
+int sum(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return n + sum(n - 1);
+}
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Sum of first %d natural numbers = %d\n", n, sum(n));
+
+    return 0;
+}
